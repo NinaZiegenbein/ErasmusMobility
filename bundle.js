c6c3b52d849a34601183adf17346b4ae0f7baa30
NinaZiegenbein <nina.ziegenbein@web.de>
2021-11-30 20:56:04 +0100

    trying to get barchart with expected value and failing

--- bundle.js
+++ bundle.js
@@ -27,7 +27,7 @@
     }
   };
 
-  const csvUrl = "/erasmus14-19_fake.csv";
+  const csvUrl = "/erasmus14-19.csv";
 
   // get the data from local file storage with d3
   const getData = async () => {
@@ -44,62 +44,112 @@
     .name("selection")
     .fields("SendCountry", "RecCountry");
 
+  vl__default["default"]
+      .mark()
+      .transform(
+          // count for x axis
+          vl__default["default"].joinaggregate([{op:"count",
+              field:"Participants",
+              as:"x_in"
+          }]).groupby(["RecCountry"]),
+          // Count for y axis
+          vl__default["default"].joinaggregate([{op:"count",
+              field:"Participants",
+              as:"y_out"
+          }]).groupby(["SendCountry"]),
+          // count for all the edges
+          vl__default["default"].joinaggregate([{op:"count",
+              field:"Participants",
+              as:"all_edges"
+          }]),
+
+          vl__default["default"].joinaggregate([{op:"count",
+              field:"Participants",
+              as:"Number"
+          }]).groupby(["SendCountry","RecCountry"]),
+          // Calculating (x*y)/all_edges getting the expectancy value for each country <3
+          vl__default["default"].calculate("(datum.x_in*datum.y_out)/datum.all_edges").as("Expectancy"),
+          vl__default["default"].calculate("round(datum.Expectancy - datum.Number)").as("Deviation")
+      );
+
+
+  /*const transformation = vl
+      .joinaggregate([{op:"count",
+          field:"Participants",
+          as:"x_in"
+      }]).groupby(["RecCountry"])
+      .joinaggregate([{op:"count",
+          field:"Participants",
+          as:"y_out"
+      }]).groupby(["SendCountry"])
+      .joinaggregate([{op:"count",
+          field:"Participants",
+          as:"all_edges"
+      }])
+      .calculate("(datum.x_in*datum.y_out)/datum.all_edges").as("Expectancy")
+      .calculate("round(datum.Expectancy - datum.Number)").as("Deviation")*/
+
   //start of matrixchart
   const matrixchart = vl__default["default"]
-    .markRect({ tooltip: true })
-    .select(selection) //changes selection in other chart
-    .transform(
-        vl__default["default"].filter("datum.Year == Year"), //filter for year according to slider
-        vl__default["default"].filter("test(regexp(Gender), datum.Gender)"), //filter for gender (radio buttons)
-        vl__default["default"].filter('datum.Duration <= Duration'),
-        vl__default["default"].filter('datum.Age <= Age'),
-      // count for x axis
-      vl__default["default"].joinaggregate([{op:"count",
-        field:"Participants",
-        as:"x_in"
-      }]).groupby(["RecCountry"]),
-      // Count for y axis
-      vl__default["default"].joinaggregate([{op:"count",
-        field:"Participants",
-        as:"y_out"
-      }]).groupby(["SendCountry"]),
-      // count for all the edges
-      vl__default["default"].joinaggregate([{op:"count",
-        field:"Participants",
-        as:"all_edges"
-      }]),
-      
-      vl__default["default"].joinaggregate([{op:"count",
-      field:"Participants",
-      as:"Number"
-      }]).groupby(["SendCountry","RecCountry"]),
-      // Calculating (x*y)/all_edges getting the expectancy value for each country <3
-      vl__default["default"].calculate("(datum.x_in*datum.y_out)/datum.all_edges").as("Expectancy"),
-      vl__default["default"].calculate("round(datum.Expectancy - datum.Number)").as("Deviation")
-    )
-    //encoding of x as Sending Country, y as Receiving Country and Color as number of participants
-    .encode(
-      vl__default["default"]
-        .x()
-        .fieldO("SendCountry")
-        .sort(vl__default["default"].field("SendCountry"))
-        .title("Sending Country")
-        .axis({ orient: "top" }),
-      vl__default["default"]
-        .y()
-        .fieldO("RecCountry")
-        .sort(vl__default["default"].field("RecCountry"))
-        .title("Receiving Country"),
-      vl__default["default"].color()
-          .fieldQ("Deviation")
-          .scale({type: "symlog", scheme: "redblue", reverse: true}) // color schemes: https://vega.github.io/vega/docs/schemes/#diverging
-          //.condition({test: "Expectancy", title:"Expectancy Value"}) //condition if doesn't work, else does
-          .title("Deviation from Expected"),
-      vl__default["default"].opacity().if(selection, vl__default["default"].value(1)).value(0.3), //change opacity when hovered
-      vl__default["default"].stroke().if(selection, vl__default["default"].value("black"))
-    );
+      .markRect({ tooltip: true })
+      .transform(
+          vl__default["default"].filter("datum.Year == Year"), //filter for year according to slider
+          vl__default["default"].filter("test(regexp(Gender), datum.Gender)"), //filter for gender (radio buttons)
+          vl__default["default"].filter('datum.Duration <= Duration'),
+          vl__default["default"].filter('datum.Age <= Age'),
+          // count for x axis
+          vl__default["default"].joinaggregate([{op:"count",
+              field:"Participants",
+              as:"x_in"
+          }]).groupby(["RecCountry"]),
+          // Count for y axis
+          vl__default["default"].joinaggregate([{op:"count",
+              field:"Participants",
+              as:"y_out"
+          }]).groupby(["SendCountry"]),
+          // count for all the edges
+          vl__default["default"].joinaggregate([{op:"count",
+              field:"Participants",
+              as:"all_edges"
+          }]),
+
+          vl__default["default"].joinaggregate([{op:"count",
+              field:"Participants",
+              as:"Number"
+          }]).groupby(["SendCountry","RecCountry"]),
+          // Calculating (x*y)/all_edges getting the expectancy value for each country <3
+          vl__default["default"].calculate("(datum.x_in*datum.y_out)/datum.all_edges").as("Expectancy"),
+          vl__default["default"].calculate("round(datum.Expectancy - datum.Number)").as("Deviation")
+      )
+      //encoding of x as Sending Country, y as Receiving Country and Color as number of participants
+      .select(selection) //changes selection in other chart
+      .encode(
+          vl__default["default"]
+              .x()
+              .fieldO("SendCountry")
+              .sort(vl__default["default"].field("SendCountry"))
+              .title("Sending Country")
+              .axis({ orient: "top" }),
+          vl__default["default"]
+              .y()
+              .fieldO("RecCountry")
+              .sort(vl__default["default"].field("RecCountry"))
+              .title("Receiving Country"),
+          vl__default["default"].color()
+              .scale({type: "symlog", scheme: "redblue", reverse: true}) // color schemes: https://vega.github.io/vega/docs/schemes/#diverging
+              //.condition({test: "Expectancy", title:"Expectancy Value"}) //condition if doesn't work, else does
+              .fieldQ("Deviation")
+              .title("Deviation from Expected"),
+          vl__default["default"].opacity().if(selection, vl__default["default"].value(1)).value(0.3), //change opacity when hovered
+          vl__default["default"].stroke().if(selection, vl__default["default"].value("black"))
+      );
+
+
+
+
+
   // bar chart for overview of number of participants over time
-  const barchart = vl__default["default"]
+  const barchartAbsolut = vl__default["default"]
     .markBar({ tooltip: true })
     .select(selection)
     .transform(vl__default["default"].filter(selection),
@@ -118,24 +168,70 @@
         .title("Participants")
     );
 
-  const viz = vl__default["default"]
-    .hconcat(matrixchart, barchart) //concatenation of visualizations
-    .params(
-      // definition of parameters valid for both visualizations
-      selection,
-      vl__default["default"].param("Year").value(2014).bind(vl__default["default"].slider(2014, 2019, 1)),
-      vl__default["default"]
-        .param("Gender")
-        .bind(
-          vl__default["default"]
-            .radio(".*", "Female", "Male", "Undefined")
-            .labels("All", "Female", "Male", "Undefined")
-        ),
-        vl__default["default"].param('Duration').value(400).bind(vl__default["default"].slider(0,400,10)),
-        vl__default["default"].param('Age').value(40).bind(vl__default["default"].slider(0, 40, 1)),
-        vl__default["default"].param('Expectancy').value(false).bind(vl__default["default"].menu(true, false))
+  // bar chart for overview of number of participants over time
+  const barchartExpectancy = vl__default["default"]
+      .markBar({ tooltip: true })
+      .transform(
+          vl__default["default"].filter("test(regexp(Gender), datum.Gender)"), //filter for gender (radio buttons)
+          vl__default["default"].filter('datum.Duration <= Duration'),
+          vl__default["default"].filter('datum.Age <= Age'),
+          // count for x axis
+          vl__default["default"].joinaggregate([{op:"count",
+              field:"Participants",
+              as:"x_in"
+          }]).groupby(["RecCountry"]),
+          // Count for y axis
+          vl__default["default"].joinaggregate([{op:"count",
+              field:"Participants",
+              as:"y_out"
+          }]).groupby(["SendCountry"]),
+          // count for all the edges
+          vl__default["default"].joinaggregate([{op:"count",
+              field:"Participants",
+              as:"all_edges"
+          }]),
 
-    );
+          vl__default["default"].joinaggregate([{op:"count",
+              field:"Participants",
+              as:"Number"
+          }]).groupby(["SendCountry","RecCountry"]),
+          // Calculating (x*y)/all_edges getting the expectancy value for each country <3
+          vl__default["default"].calculate("(datum.x_in*datum.y_out)/datum.all_edges").as("Expectancy"),
+          vl__default["default"].calculate("round(datum.Expectancy - datum.Number)").as("Deviation"),
+          vl__default["default"].filter(selection),
+      )
+      .select(selection)
+      //encoding of y axis as Year and x axis as deviation from expectancy
+      .encode(
+          vl__default["default"].y().fieldN("Year").title("Year"),
+          vl__default["default"]
+              .x()
+              //.aggregate("max")
+              .fieldQ("Deviation")
+              .sort("ascending")
+              .title("Deviation from Expectancy")
+      );
+
+  const viz = vl__default["default"]
+      .hconcat(matrixchart, vl__default["default"].vconcat(barchartAbsolut, barchartExpectancy)) //concatenation of visualizations
+      .params(
+          // definition of parameters valid for both visualizations
+          selection,
+          vl__default["default"].param("Year").value(2014).bind(vl__default["default"].slider(2014, 2019, 1)),
+          vl__default["default"]
+              .param("Gender")
+              .bind(
+                  vl__default["default"]
+                      .radio(".*", "Female", "Male", "Undefined")
+                      .labels("All", "Female", "Male", "Undefined")
+              ),
+          vl__default["default"].param('Duration').value(400).bind(vl__default["default"].slider(0,400,10)),
+          vl__default["default"].param('Age').value(40).bind(vl__default["default"].slider(0, 40, 1)),
+          vl__default["default"].param('Expectancy').value(false).bind(vl__default["default"].menu(true, false))
+
+      );
+
+
 
   /*viewof durationRange = rangeSlider({
     min: d3.min(d => d.Duration),
--- js/viz.js
+++ js/viz.js
@@ -8,62 +8,112 @@ const selection = vl
   .name("selection")
   .fields("SendCountry", "RecCountry");
 
+const transformation = vl
+    .mark()
+    .transform(
+        // count for x axis
+        vl.joinaggregate([{op:"count",
+            field:"Participants",
+            as:"x_in"
+        }]).groupby(["RecCountry"]),
+        // Count for y axis
+        vl.joinaggregate([{op:"count",
+            field:"Participants",
+            as:"y_out"
+        }]).groupby(["SendCountry"]),
+        // count for all the edges
+        vl.joinaggregate([{op:"count",
+            field:"Participants",
+            as:"all_edges"
+        }]),
+
+        vl.joinaggregate([{op:"count",
+            field:"Participants",
+            as:"Number"
+        }]).groupby(["SendCountry","RecCountry"]),
+        // Calculating (x*y)/all_edges getting the expectancy value for each country <3
+        vl.calculate("(datum.x_in*datum.y_out)/datum.all_edges").as("Expectancy"),
+        vl.calculate("round(datum.Expectancy - datum.Number)").as("Deviation")
+    )
+
+
+/*const transformation = vl
+    .joinaggregate([{op:"count",
+        field:"Participants",
+        as:"x_in"
+    }]).groupby(["RecCountry"])
+    .joinaggregate([{op:"count",
+        field:"Participants",
+        as:"y_out"
+    }]).groupby(["SendCountry"])
+    .joinaggregate([{op:"count",
+        field:"Participants",
+        as:"all_edges"
+    }])
+    .calculate("(datum.x_in*datum.y_out)/datum.all_edges").as("Expectancy")
+    .calculate("round(datum.Expectancy - datum.Number)").as("Deviation")*/
+
 //start of matrixchart
 const matrixchart = vl
-  .markRect({ tooltip: true })
-  .select(selection) //changes selection in other chart
-  .transform(
-      vl.filter("datum.Year == Year"), //filter for year according to slider
-      vl.filter("test(regexp(Gender), datum.Gender)"), //filter for gender (radio buttons)
-      vl.filter('datum.Duration <= Duration'),
-      vl.filter('datum.Age <= Age'),
-    // count for x axis
-    vl.joinaggregate([{op:"count",
-      field:"Participants",
-      as:"x_in"
-    }]).groupby(["RecCountry"]),
-    // Count for y axis
-    vl.joinaggregate([{op:"count",
-      field:"Participants",
-      as:"y_out"
-    }]).groupby(["SendCountry"]),
-    // count for all the edges
-    vl.joinaggregate([{op:"count",
-      field:"Participants",
-      as:"all_edges"
-    }]),
-    
-    vl.joinaggregate([{op:"count",
-    field:"Participants",
-    as:"Number"
-    }]).groupby(["SendCountry","RecCountry"]),
-    // Calculating (x*y)/all_edges getting the expectancy value for each country <3
-    vl.calculate("(datum.x_in*datum.y_out)/datum.all_edges").as("Expectancy"),
-    vl.calculate("round(datum.Expectancy - datum.Number)").as("Deviation")
-  )
-  //encoding of x as Sending Country, y as Receiving Country and Color as number of participants
-  .encode(
-    vl
-      .x()
-      .fieldO("SendCountry")
-      .sort(vl.field("SendCountry"))
-      .title("Sending Country")
-      .axis({ orient: "top" }),
-    vl
-      .y()
-      .fieldO("RecCountry")
-      .sort(vl.field("RecCountry"))
-      .title("Receiving Country"),
-    vl.color()
-        .fieldQ("Deviation")
-        .scale({type: "symlog", scheme: "redblue", reverse: true}) // color schemes: https://vega.github.io/vega/docs/schemes/#diverging
-        //.condition({test: "Expectancy", title:"Expectancy Value"}) //condition if doesn't work, else does
-        .title("Deviation from Expected"),
-    vl.opacity().if(selection, vl.value(1)).value(0.3), //change opacity when hovered
-    vl.stroke().if(selection, vl.value("black"))
-  );
+    .markRect({ tooltip: true })
+    .transform(
+        vl.filter("datum.Year == Year"), //filter for year according to slider
+        vl.filter("test(regexp(Gender), datum.Gender)"), //filter for gender (radio buttons)
+        vl.filter('datum.Duration <= Duration'),
+        vl.filter('datum.Age <= Age'),
+        // count for x axis
+        vl.joinaggregate([{op:"count",
+            field:"Participants",
+            as:"x_in"
+        }]).groupby(["RecCountry"]),
+        // Count for y axis
+        vl.joinaggregate([{op:"count",
+            field:"Participants",
+            as:"y_out"
+        }]).groupby(["SendCountry"]),
+        // count for all the edges
+        vl.joinaggregate([{op:"count",
+            field:"Participants",
+            as:"all_edges"
+        }]),
+
+        vl.joinaggregate([{op:"count",
+            field:"Participants",
+            as:"Number"
+        }]).groupby(["SendCountry","RecCountry"]),
+        // Calculating (x*y)/all_edges getting the expectancy value for each country <3
+        vl.calculate("(datum.x_in*datum.y_out)/datum.all_edges").as("Expectancy"),
+        vl.calculate("round(datum.Expectancy - datum.Number)").as("Deviation")
+    )
+    //encoding of x as Sending Country, y as Receiving Country and Color as number of participants
+    .select(selection) //changes selection in other chart
+    .encode(
+        vl
+            .x()
+            .fieldO("SendCountry")
+            .sort(vl.field("SendCountry"))
+            .title("Sending Country")
+            .axis({ orient: "top" }),
+        vl
+            .y()
+            .fieldO("RecCountry")
+            .sort(vl.field("RecCountry"))
+            .title("Receiving Country"),
+        vl.color()
+            .scale({type: "symlog", scheme: "redblue", reverse: true}) // color schemes: https://vega.github.io/vega/docs/schemes/#diverging
+            //.condition({test: "Expectancy", title:"Expectancy Value"}) //condition if doesn't work, else does
+            .fieldQ("Deviation")
+            .title("Deviation from Expected"),
+        vl.opacity().if(selection, vl.value(1)).value(0.3), //change opacity when hovered
+        vl.stroke().if(selection, vl.value("black"))
+    );
+
+
+
+
+
 // bar chart for overview of number of participants over time
-const barchart = vl
+const barchartAbsolut = vl
   .markBar({ tooltip: true })
   .select(selection)
   .transform(vl.filter(selection),
@@ -82,24 +132,70 @@ const barchart = vl
       .title("Participants")
   );
 
-export const viz = vl
-  .hconcat(matrixchart, barchart) //concatenation of visualizations
-  .params(
-    // definition of parameters valid for both visualizations
-    selection,
-    vl.param("Year").value(2014).bind(vl.slider(2014, 2019, 1)),
-    vl
-      .param("Gender")
-      .bind(
-        vl
-          .radio(".*", "Female", "Male", "Undefined")
-          .labels("All", "Female", "Male", "Undefined")
-      ),
-      vl.param('Duration').value(400).bind(vl.slider(0,400,10)),
-      vl.param('Age').value(40).bind(vl.slider(0, 40, 1)),
-      vl.param('Expectancy').value(false).bind(vl.menu(true, false))
+// bar chart for overview of number of participants over time
+const barchartExpectancy = vl
+    .markBar({ tooltip: true })
+    .transform(
+        vl.filter("test(regexp(Gender), datum.Gender)"), //filter for gender (radio buttons)
+        vl.filter('datum.Duration <= Duration'),
+        vl.filter('datum.Age <= Age'),
+        // count for x axis
+        vl.joinaggregate([{op:"count",
+            field:"Participants",
+            as:"x_in"
+        }]).groupby(["RecCountry"]),
+        // Count for y axis
+        vl.joinaggregate([{op:"count",
+            field:"Participants",
+            as:"y_out"
+        }]).groupby(["SendCountry"]),
+        // count for all the edges
+        vl.joinaggregate([{op:"count",
+            field:"Participants",
+            as:"all_edges"
+        }]),
 
-  );
+        vl.joinaggregate([{op:"count",
+            field:"Participants",
+            as:"Number"
+        }]).groupby(["SendCountry","RecCountry"]),
+        // Calculating (x*y)/all_edges getting the expectancy value for each country <3
+        vl.calculate("(datum.x_in*datum.y_out)/datum.all_edges").as("Expectancy"),
+        vl.calculate("round(datum.Expectancy - datum.Number)").as("Deviation"),
+        vl.filter(selection),
+    )
+    .select(selection)
+    //encoding of y axis as Year and x axis as deviation from expectancy
+    .encode(
+        vl.y().fieldN("Year").title("Year"),
+        vl
+            .x()
+            //.aggregate("max")
+            .fieldQ("Deviation")
+            .sort("ascending")
+            .title("Deviation from Expectancy")
+    );
+
+export const viz = vl
+    .hconcat(matrixchart, vl.vconcat(barchartAbsolut, barchartExpectancy)) //concatenation of visualizations
+    .params(
+        // definition of parameters valid for both visualizations
+        selection,
+        vl.param("Year").value(2014).bind(vl.slider(2014, 2019, 1)),
+        vl
+            .param("Gender")
+            .bind(
+                vl
+                    .radio(".*", "Female", "Male", "Undefined")
+                    .labels("All", "Female", "Male", "Undefined")
+            ),
+        vl.param('Duration').value(400).bind(vl.slider(0,400,10)),
+        vl.param('Age').value(40).bind(vl.slider(0, 40, 1)),
+        vl.param('Expectancy').value(false).bind(vl.menu(true, false))
+
+    );
+
+
 
 /*viewof durationRange = rangeSlider({
   min: d3.min(d => d.Duration),
